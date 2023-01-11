3072deade55e857876efd3770d93baa66bd30f66
mrkdddddddd <102356007+mrkdddddddd@users.noreply.github.com>
2023-01-11 18:02:05 +0900

    test laptop

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:aka_project/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//test laptop
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
